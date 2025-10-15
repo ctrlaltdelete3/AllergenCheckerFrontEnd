e505ec52e3b46cff94a9111e808730343639c334
ctrlaltdelete3 <ivana.bevanda1@gmail.com>
2025-10-15 10:38:11 +0200

    changes to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,55 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("currentUser");
+    navigate("/");
+  };
   return (
     <nav className="navbar">
-      <h3>Allergen Checker App</h3>
-      <div className="links">
-        <Link to="/">Home</Link>
+      <div className="nav-left">
+        <Link to="/">Allergen CHecker</Link>
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+        </li>
+        {currentUser && (
+          <>
+            <li>
+              <NavLink to="/myallergens">My Allergens</NavLink>
+            </li>
+            <li>
+              <NavLink to="/checkproduct">Check Product</NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+
+      <div className="nav-right">
+        {currentUser ? (
+          <>
+            <span className="user">Hello, {currentUser.email}</span>
+            <button className="btn" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <NavLink className="btn secondary" to="/login">
+              Login
+            </NavLink>
+            <NavLink className="btn" to="/newuser">
+              Register
+            </NavLink>
+          </>
+        )}
       </div>
     </nav>
   );
